b5dc34d9dc0e9d6793738a93afd705a35a8f595b
gflores-sleek <gael@sleek-corp.com>
2020-02-03 19:14:47 +0800

    [auto] Generating Production Files...

--- app/src/pages/client/c-portal.vue
+++ app/src/pages/client/c-portal.vue
@@ -334,11 +334,15 @@ export default {
     waitingConvos() {
       let res = this.convos.filter(c => c.state == "waiting");
       if (res.length > 0) {
-        webNotification.showNotification('Someone calling...', {
-          body: `${res.length} ${res.length == 1 ? "visitor" : "visitors"} sent a request to connect`,
-          tag: `${notificationSeed}/${res.length}`,
-          vibrate: [100, 160, 220, 320]
-        });
+        try {
+          webNotification.showNotification('Someone calling...', {
+            body: `${res.length} ${res.length == 1 ? "visitor" : "visitors"} sent a request to connect`,
+            tag: `${notificationSeed}/${res.length}`,
+            vibrate: [100, 160, 220, 320]
+          });
+        } catch (e) {
+        }
+        
       }
       return res;
     },
