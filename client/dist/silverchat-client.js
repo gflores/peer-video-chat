ad7b18964e6c6c0110a7e82ab5a0bc2494b6acbe
gflores-sleek <gael@sleek-corp.com>
2020-02-29 16:26:32 +0800

    [auto] Generating Production Files...

--- client/dist/silverchat-client.js
+++ client/dist/silverchat-client.js
@@ -1,15 +1,15 @@
-let host = 'http://localhost:3015'
+let host = 'https://client.silverchat.co'
 
 let links = [
   `<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700&display=swap" rel=stylesheet>`,
   `<link href=${host}/css/app.3412c663.css rel=preload as=style>`,
-  `<link href=${host}/js/app.9698e09c.js rel=preload as=script>`,
-  `<link href=${host}/js/chunk-vendors.cf46f65b.js rel=preload as=script>`,
+  `<link href=${host}/js/app.2a55e9af.js rel=preload as=script>`,
+  `<link href=${host}/js/chunk-vendors.b1c704fb.js rel=preload as=script>`,
   `<link href=${host}/css/app.3412c663.css rel=stylesheet>`,
 ]
 let scripts = [
-  `${host}/js/chunk-vendors.cf46f65b.js`,
-  `${host}/js/app.9698e09c.js`,
+  `${host}/js/chunk-vendors.b1c704fb.js`,
+  `${host}/js/app.2a55e9af.js`,
 ]
 
 links.forEach(link => {
